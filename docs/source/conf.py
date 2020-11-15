7e4ddcc3dafb786077a81c1e1d4f92497ebf7ef1
Nikhil Verma <nikhilweee@gmail.com>
2020-11-15 14:21:24 +0530

    Updated version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Nikhil Verma'
 author = 'Nikhil Verma'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="shis",
-    version="0.0.8",
+    version="0.0.9",
     author="Nikhil Verma",
     author_email="nikhilweee@gmail.com",
     description="Simple HTTP Image Server",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/nikhilweee/shis",
+    url="https://github.com/nikhilweee/shis/",
     packages=setuptools.find_packages(),
     package_data={'shis': ['templates/*', 'templates/*/*']},
     license="MIT",
